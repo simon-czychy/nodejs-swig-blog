a173729e23a542e7c548f2030fd85b63cea7444f
Simon Czychy <simon.czychy@googlemail.com>
2016-09-14 10:42:18 +0200

    POST or GET to send Data to a Twig Template

--- routes/IndexRoute.js
+++ routes/IndexRoute.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Node With Express and Pug', message: 'Welcome to Nodejs With Express and Pug' });
+  res.render('index.php', {
+    get : 
+    {
+      title: 'Node With Express and Pug', 
+      message: 'Welcome to Nodejs With Express and Pug' 
+    }
+  });
 });
 
 module.exports = router;
