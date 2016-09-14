154a165068d5b4ab49307e232e4716c8584f2e66
simon-czychy <simon.czychy@googlemail.com>
2016-09-14 14:00:48 +0200

    Added mongodb package

--- app.js
+++ app.js
@@ -43,6 +43,24 @@ app.use(function(err, req, res, next) {
   });
 });
 
+//Database
+
+var MongoClient = require('mongodb').MongoClient,
+    assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  db.close();
+});
+
+
+
 //export
 module.exports = app;
 
