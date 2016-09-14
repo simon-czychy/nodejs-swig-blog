02480ed61dd1b6767dea63de44f42469fcf9f658
simon-czychy <simon.czychy@googlemail.com>
2016-09-14 16:01:18 +0200

    Passing all GET parameters to error.php for more detailed informations

--- application/error.php
+++ application/error.php
@@ -3,6 +3,6 @@
 include_once "./modules/TwigRenderer.php";
 
 $errorView = new TwigRenderer();
-$errorView->render("error.html.twig", array('message'=> $_GET['message']));
+$errorView->render("error.html.twig", $_GET);
 
 ?>
