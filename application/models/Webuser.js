7c366998f5fc911a782c8fc4365d00daefbbacad
simon-czychy <simon.czychy@googlemail.com>
2016-10-25 18:10:42 +0200

    Refactored Login, error messages, hashed passwords

---
+++ application/models/Webuser.js
@@ -0,0 +1,150 @@
+var crypto = require('crypto'),
+    dbConfig = require('../modules/config').dbConfig,
+    rdb = require('rethinkdb'),
+    connectionPooling = false,
+    connectionPool = null,
+    config = require("../modules/config").Config;
+
+
+//if pool connection is configured, use it
+if (typeof dbConfig.pool === 'object') {
+  var pool = require('generic-pool');
+  connectionPooling = true;
+
+  connectionPool = pool.Pool({
+    name: 'rethinkdb',
+    max : dbConfig.pool.max || 1000,
+    min : dbConfig.pool.min || 2,
+    log : dbConfig.pool.log || true,
+    idleTimeoutMillis : dbConfig.pool.idleTimeoutMillis || 1 * 60 * 1000,
+    reapIntervalMillis: dbConfig.pool.reapIntervalMillis || 30 * 1000,
+
+    create: function(callback) {
+      rdb.connect({host: dbConfig.host, port: dbConfig.port}, function(err, connection) {
+          if(err) {
+            var errMsg = util.format("Failed connecting to RethinkDB instance on {host: %s, port: %s}", dbConfig.host, dbConfig.port);
+            console.log("[ERROR]: " + errMsg);
+            return callback(new Error(errMsg));
+          }
+          connection._id = Math.floor(Math.random()*10001);
+          connection.use(dbConfig.db);
+          console.log("[DEBUG]: Connection created: %s", connection._id);
+          callback(null, connection);
+      });
+    },
+
+    destroy: function(connection) {
+      console.log("[DEBUG]: Connection closed: %s", connection._id);
+      connection.close();
+    }
+  });
+
+}
+
+
+function onConnection(callback) {
+  if(connectionPooling) {
+    connectionPool.acquire(function(err, connection) {
+      if(err) {
+        callback(err);
+      }
+      else {
+        console.log("[INFO]: Pooled connection: %s", connection._id);
+        callback(null, connection);
+      }
+    });
+  }
+  else {
+    rdb.connect({host: dbConfig.host, port: dbConfig.port}, function(err, connection) {
+      if(err) {
+        console.log("[ERROR]: Cannot connect to RethinkDB database: %s on port %s", dbConfig['host'], dbConfig['port']);
+        callback(err);
+      }
+      else {
+        connection._id = Math.floor(Math.random()*10001);
+        connection.use(dbConfig.db);
+        console.log("[DEBUG]: Connection established. ID: %s", connection._id);
+        callback(null, connection);
+      }
+    });
+  }
+}
+
+function release(connection) {
+  console.log("[DEBUG]: Disconnecting connection: %s", connection._id);
+  if(connectionPooling) {
+    connectionPool.release(connection);
+  }
+  else {
+    connection.close();
+  }
+}
+
+
+
+exports.Login = function(user, password, callback, res) {
+  console.log("Login: %s, %s", user, password);
+
+  onConnection(function(err,connection) {
+    if(err) {
+      console.log("[ERROR][manualLogin]: %s:%s\n%s", err.name, err.msg, err.message);
+      callback(null);
+      return;
+    }
+
+    rdb.table("users").filter(rdb.row("email").eq(user)).limit(1).run(connection, function(err, cursor) {
+      if(err) {
+        console.log("[ERROR][manualLogin]: %s:%s\n%s", err.name, err.msg, err.message);
+        callback(null);
+      }
+      else {
+        var statusMessage = "unkown-error";
+        cursor.next(function(err, row) {
+          if(err) {
+            console.log("[ERROR][manualLogin][CURSOR]: %s:%s\n%s", err.name, err.msg, err.message);
+            release(connection);
+            statusMessage = "user-not-found";
+          }
+          else {
+              validatePassword(password, row.password, function(err, passCorrect) {
+              if(passCorrect) {
+                callback(null, row);
+                statusMessage = "login-successful";
+              }
+              else {
+
+                callback('login-failed');
+                statusMessage = "bad-pass";
+              }
+              release(connection);
+            });
+          }
+          res.send(statusMessage);
+        });
+      }
+    });
+  });
+}
+
+//==================================== PRIVATE METHODS ===================================//
+
+ var generateSalt = function() {
+   return crypto.randomBytes(128).toString('hex');
+ }
+
+ var md5 = function(str) {
+   return crypto.createHash('md5').update(str).digest('hex');
+ }
+
+ var saltAndHash = function(pass, callback)
+{
+  var salt = generateSalt();
+  callback(salt + md5(pass + salt));
+}
+
+var validatePassword = function(plainPass, hashedPass, callback)
+{
+  var salt = hashedPass.substr(0, 10);
+  var validHash = salt + md5(plainPass + salt);
+  callback(null, hashedPass === validHash);
+}
--- routes/LoginRoute.js
+++ routes/LoginRoute.js
@@ -2,12 +2,9 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
-var rdbHelper = require('../DataBaseHelper');
 var rdb = require("rethinkdb");
-
-
-
-rdbHelper.connect();
+var Webuser  = require("../application/models/Webuser");
+var config = require("../application/modules/config").Config;
 
 const ERROR_USERNAME_NOT_FOUND = "Email not found!";
 const ERROR_USERNAME_INVALID = "Email invalid!";
@@ -16,50 +13,18 @@ const ERROR_USERNAME_TOO_SHORT = "Email must have at least 3 characters!"
 
 
 router.post('/', urlencodedParser, function (req,res,next) {
-  if (!req.body) return res.sendStatus(400);
-  login(req, res, req.body.username, req.body.password);
-
-});
-
-function login(req, res, email, password) {
-  //check email
-  rdb.table("users").filter(rdb.row("email").eq(email)).filter(rdb.row("password").eq(password)).run(connection, function(err, cursor) {
-    if(err) {
-      throw err;
+  if (!req.body) {
+    return res.status(400).send().end();
+  }
+  Webuser.Login(req.body.username, req.body.password, function(connection, user) {
+    if(!user || typeof user == "undefined") {
+      //res.send("Something wrent wrong!");
     }
     else {
-      cursor.toArray(function(err, result) {
-        if(err) {
-          throw err;
-        }
-        else {
-          var row = result[0];
-          if(result.length == 1) {
-            if(row !== "" && row !== null && row !== "undefined") {
-              res.cookie("userid", row.id, 1, { domain: ".localhost",maxAge: 3600});
-              res.cookie("email", row.email, 1, { domain: ".localhost",maxAge: 3600});
-              res.send("done");
-            }
-          }
-          else {
-             res.send("Email/Password combination doesn't exists!");
-          }
-        }
-      });
+        res.cookie("userid", user.id, 1, { domain: config.host, maxAge: 3600});
+        res.cookie("email", user.email, 1, { domain: config.host, maxAge: 3600});
     }
-  });
-
-  
-}
-
-function hash(string) {
-  var crypto = require('crypto'),
-      salt = crypto.randomBytes(128).toString('base64'), key;
-
-  crypto.pbkdf2( string, salt, 10000, 512, function(err, dk) { key = dk; } );
-  console.log(key);
-}
-
-
+  }, res );
+});
 
 module.exports = router;
