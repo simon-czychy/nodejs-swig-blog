59d4ba47c206534ad59ef70ca78a6771608da7f6
simon-czychy <simon.czychy@googlemail.com>
2016-10-25 18:16:22 +0200

    Merge branch Login into master

---
+++ application/modules/config.js
@@ -0,0 +1,17 @@
+module.exports.Config = {
+  host: 'localhost'
+}
+
+
+module.exports.dbConfig = {
+  host: process.env.RDB_HOST || 'localhost',
+  port: parseInt(process.env.RDB_PORT) || 28015,
+  db  : process.env.RDB_DB || 'blog',
+  pool: {
+    min: 5,
+    max: 100,
+    log: true,
+    idleTimeoutMillis : 1 * 60 * 1000, // 1 minute
+    reapIntervalMillis: 30 * 1000,  // 30 seconds
+  }
+}
