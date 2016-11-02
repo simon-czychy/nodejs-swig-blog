dbe6fc43ccaf986e6041c1852b55dc32e2a18776
simon-czychy <simon.czychy@googlemail.com>
2016-11-02 16:26:46 +0100

    Added viws, added styles, added tinymce

--- routes/AdminRoute.js
+++ routes/AdminRoute.js
@@ -10,7 +10,10 @@ rdbHelper.connect();
 router.get('/', function(req, res, next) {
 
   if(req.cookies.userid && req.cookies.email) {
-    isCookieValid(res, req.cookies.userid, req.cookies.email);
+    isCookieValid(res, req.cookies.userid, req.cookies.email, 'admin/admin',  {
+      title: 'Blog Sausage',
+      isloggedin: loggedin
+    });
   }
   else {
     loggedin = false;
@@ -22,7 +25,27 @@ router.get('/', function(req, res, next) {
   }
 });
 
-function isCookieValid(res, userid, email) {
+/* GET admin page. */
+router.get('/article/add', function(req, res, next) {
+
+  if(req.cookies.userid && req.cookies.email) {
+    isCookieValid(res, req.cookies.userid, req.cookies.email, 'admin/article/add',  {
+      title: 'Blog Sausage',
+      isloggedin: loggedin,
+      host: req.headers.host
+    });
+  }
+  else {
+    loggedin = false;
+    res.render('index',  {
+      title: 'Blog Sausage',
+      message: 'Here you can find some awesome Blog posts!',
+      isloggedin: loggedin
+    });
+  }
+});
+
+function isCookieValid(res, userid, email, view, options) {
   rdb.table("users").filter(rdb.row("id").eq(userid)).filter(rdb.row("email").eq(email)).run(connection, function(err, cursor) {
     if(err) {
       throw err;
@@ -37,10 +60,7 @@ function isCookieValid(res, userid, email) {
           if(result.length == 1) {
             if(row !== "" && row !== null && row !== "undefined") {
               loggedin = true;
-              res.render('admin/admin',  {
-                title: 'Blog Sausage',
-                isloggedin: loggedin
-              });
+              res.render(view, options);
             }
           }
         }
